c413a9f4cf321841c66ebb82aec16fa4d88631fe
yazan alshekha <yazan.alshekha@outlook.com>
2022-09-11 13:55:45 +0300

    delete item from the cart is done

--- src/store/cart.js
+++ src/store/cart.js
@@ -9,14 +9,28 @@ export default ( state = initialState , action)=>{
         case "ADD_TO_CART":
         let cart=state.filter((item)=> item.name !== payload.name)
         return [...cart,payload];
-    default :
-        return state;
+
+        case 'DELETE_ITEM_FROM_CART':
+            let stateAfterDelete=state.filter( (item) => item.name!== payload);
+            return [...stateAfterDelete];
+
+        default :
+            return state;
     }
 }
 
+// function to add new item to the cart
 export function add(name){
     return{
         type:"ADD_TO_CART",
+        payload:name,
+    }
+}
+
+// function to delete an item fro the cart
+export function delete_item(name){
+    return{
+        type:"DELETE_ITEM_FROM_CART",
         payload:name,
     }
 }
